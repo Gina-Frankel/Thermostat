933fcdf5c9743e2f6c13a00c7d4ef8b9a113a663
Gina Frankel <g.frankel22@gmail.com>
2020-09-01 18:33:32 +0100

    Remove comments + add conditional to changePowerSaveMode

--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,86 +1,82 @@
-'use strict';
+"use strict";
 
-const MAX_TEMPERATURE_PSM_ON = 25
-const MIN_TEMPERATURE = 10
-const MAX_TEMPERATURE_PSM_OFF = 32
-const LOW_USAGE_LIMIT = 18
-const HIGH_USAGE_LIMIT = 24
-//var usage
-
+const MAX_TEMPERATURE_PSM_ON = 25;
+const MIN_TEMPERATURE = 10;
+const MAX_TEMPERATURE_PSM_OFF = 32;
+const LOW_USAGE_LIMIT = 18;
+const HIGH_USAGE_LIMIT = 24;
 
 function Thermostat() {
-  // this.temperature = temperature
-  this.powerSaveMode = true
-  this.startTemperature = 20
-  this.temperature = this.startTemperature
-  this.usage = 'medium-usage'
-};
-
-
-
-Thermostat.prototype.changePowerSaveMode = function() {
-  this.powerSaveMode ? this.powerSaveMode = false : 
   this.powerSaveMode = true;
-  
+  this.startTemperature = 20;
+  this.temperature = this.startTemperature;
+  this.usage = "medium-usage";
 }
+
+Thermostat.prototype.changePowerSaveMode = function () {
+  this.powerSaveMode
+    ? (this.powerSaveMode = false)
+    : (this.powerSaveMode = true);
+  if (this.powerSaveMode == true) {
+    this.temperature = 25;
+  }
+};
 
 Thermostat.prototype.reset = function () {
   this.powerSaveMode = true;
-  this.temperature = this.startTemperature
-}
+  this.temperature = this.startTemperature;
+};
 
-/*Thermostat.prototype.getUsage = function() {
-  if (this.temperature < 18 ) {
-    return 'low-usage'
-  }
-}
-*/
-Thermostat.prototype.changeUsage = function() {
+Thermostat.prototype.changeUsage = function () {
   if (this.temperature < LOW_USAGE_LIMIT) {
-    this.usage = 'low-usage'
-  } else if (this.temperature > HIGH_USAGE_LIMIT ) {
-    this.usage = 'high-usage'
+    this.usage = "low-usage";
+  } else if (this.temperature > HIGH_USAGE_LIMIT) {
+    this.usage = "high-usage";
   } else {
-    this.usage = 'medium-usage'
+    this.usage = "medium-usage";
   }
-}
+};
 
-Thermostat.prototype.isMaximumTemperature = function() {
-  if (this.powerSaveMode ===true && this.temperature > MAX_TEMPERATURE_PSM_ON) {
-    return "MAX_TEMPERATURE_PSM_ON"
-  } 
-  if (this.powerSaveMode === false && this.temperature > MAX_TEMPERATURE_PSM_OFF ) {
-    return "MAX_TEMPERATURE_PSM_OFF"
+Thermostat.prototype.isMaximumTemperature = function () {
+  if (
+    this.powerSaveMode === true &&
+    this.temperature > MAX_TEMPERATURE_PSM_ON
+  ) {
+    return "MAX_TEMPERATURE_PSM_ON";
   }
-}
-
-Thermostat.prototype.isMinimumTemperature = function() {
-  if (this.temperature  < MIN_TEMPERATURE) {
-    return true
+  if (
+    this.powerSaveMode === false &&
+    this.temperature > MAX_TEMPERATURE_PSM_OFF
+  ) {
+    return "MAX_TEMPERATURE_PSM_OFF";
   }
+};
 
-}
+Thermostat.prototype.isMinimumTemperature = function () {
+  if (this.temperature < MIN_TEMPERATURE) {
+    return true;
+  }
+};
 
-Thermostat.prototype.upTemperature = function() {
-  this.temperature = this.temperature += 1
-    if (this.isMaximumTemperature() === "MAX_TEMPERATURE_PSM_ON"){
-      this.temperature = MAX_TEMPERATURE_PSM_ON
-    }
-    if (this.isMaximumTemperature() === "MAX_TEMPERATURE_PSM_OFF") {
-        this.temperature = MAX_TEMPERATURE_PSM_OFF
-    }
-    this.changeUsage()
-}
+Thermostat.prototype.upTemperature = function () {
+  this.temperature = this.temperature += 1;
+  if (this.isMaximumTemperature() === "MAX_TEMPERATURE_PSM_ON") {
+    this.temperature = MAX_TEMPERATURE_PSM_ON;
+  }
+  if (this.isMaximumTemperature() === "MAX_TEMPERATURE_PSM_OFF") {
+    this.temperature = MAX_TEMPERATURE_PSM_OFF;
+  }
+  this.changeUsage();
+};
 
+Thermostat.prototype.downTemperature = function () {
+  this.temperature = this.temperature - 1;
+  if (this.isMinimumTemperature() === true) {
+    this.temperature = MIN_TEMPERATURE;
+  }
+  this.changeUsage();
+};
 
-Thermostat.prototype.downTemperature = function() {
-  this.temperature = this.temperature - 1
-if (this.isMinimumTemperature() === true) {
-  this.temperature = MIN_TEMPERATURE }
-  this.changeUsage()
-  
-}
-
-Thermostat.prototype.showUsage = function() {
-  return (this.usage)
-}
+Thermostat.prototype.showUsage = function () {
+  return this.usage;
+};
